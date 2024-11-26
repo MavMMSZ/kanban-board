4bd25d27a82e4c9e8a814bc3f61d9340180b501e
Maverick S <maverickmmsc@gmail.com>
2024-11-26 15:09:25 -0500

    last second fixes

--- server/dist/middleware/auth.js
+++ server/dist/middleware/auth.js
@@ -1,17 +1,25 @@
 import jwt from 'jsonwebtoken';
+// Middleware function to authenticate JWT token
 export const authenticateToken = (req, res, next) => {
-    // TODO: verify the token exists and add the user data to the request object
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) {
-        res.sendStatus(401);
-        return;
+    // Get the authorization header from the request
+    const authHeader = req.headers.authorization;
+    // Check if the authorization header is present
+    if (authHeader) {
+        // Extract the token from the authorization header
+        const token = authHeader.split(' ')[1];
+        // Get the secret key from the environment variables
+        const secretKey = process.env.JWT_SECRET_KEY || '';
+        // Verify the JWT token
+        jwt.verify(token, secretKey, (err, user) => {
+            if (err) {
+                return res.sendStatus(403); // Send forbidden status if the token is invalid
+            }
+            // Attach the user information to the request object
+            req.user = user;
+            return next(); // Call the next middleware function
+        });
     }
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-        if (err) {
-            return res.sendStatus(403);
-        }
-        req.user = user;
-        return next();
-    });
+    else {
+        res.sendStatus(401); // Send unauthorized status if no authorization header is present
+    }
 };
